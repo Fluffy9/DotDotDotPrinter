611253c3fec5ac160a9d2bc2edee1b962ac45791
Fluffy9 <Fluffy9@users.noreply.github.com>
2022-05-17 20:13:32 -0400

    Change path

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,15 @@
 const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
 module.exports = {
-	css: {
+    css: {
 		loaderOptions: {
 			sass: {
-				additionalData: bootstrapSassAbstractsImports.join('\n')
+				additionalData: '@import "~bootstrap/scss/_functions.scss"\n@import "~@/assets/scss/vendors/bootstrap-vue/_custom.scss"\n@import "~bootstrap/scss/_variables.scss"\n@import "~bootstrap/scss/_mixins.scss"\n@import "~bootstrap-vue/src/_variables.scss"'
 			},
 			scss: {
-				additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
+				additionalData: '@import "~bootstrap/scss/_functions.scss";\n@import "~@/assets/scss/vendors/bootstrap-vue/_custom.scss";\n@import "~bootstrap/scss/_variables.scss";\n@import "~bootstrap/scss/_mixins.scss";\n@import "~bootstrap-vue/src/_variables.scss";\n'
 			}
 		}
-	}
+	},
+
+    publicPath: '/DotDotDotPrinter/'
 }
